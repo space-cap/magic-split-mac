51c167afcfd8f0d406fda491564472d7528deeae
이영현 <hong.git@yahoo.com>
2026-05-15 18:26:17 +0900

    안녕

--- lib/magic_split_mac_web/live/hello_live.ex
+++ lib/magic_split_mac_web/live/hello_live.ex
@@ -1,19 +1,18 @@
 defmodule MagicSplitMacWeb.HelloLive do
   use MagicSplitMacWeb, :live_view
 
+  @impl true
   def mount(_params, _session, socket) do
-    # 페이지가 처음 열릴 때 실행되는 함수입니다.
-    # 여기에 초기 데이터를 넣을 수 있어요.
     {:ok, assign(socket, :message, "Hello World from New Page!")}
   end
 
+  @impl true
   def render(assigns) do
-    # 화면에 그려질 HTML 내용입니다.
     ~H"""
     <Layouts.app flash={@flash}>
       <div class="flex flex-col items-center justify-center min-h-[60vh] text-center">
         <div class="p-10 bg-base-200 rounded-3xl shadow-xl border border-primary/20">
-          <h1 class="text-6xl font-black text-primary mb-6 animate-pulse">
+          <h1 class="text-6xl font-black text-primary mb-6 animate-bounce">
             {@message}
           </h1>
           <p class="text-xl text-base-content/70 mb-8">
@@ -24,7 +23,7 @@ defmodule MagicSplitMacWeb.HelloLive do
             class="btn btn-primary btn-lg"
           >
             홈으로 돌아가기
-          </link>
+          </.link>
         </div>
       </div>
     </Layouts.app>
